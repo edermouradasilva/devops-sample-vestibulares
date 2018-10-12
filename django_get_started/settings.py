1f6118c1dfbc9f0d05b9a45a41d04da02a78600d
Éder Moura da Silva <42222940+edermouradasilva@users.noreply.github.com>
2018-10-11 23:36:06 -0300

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -3,6 +3,11 @@ Django settings for django_get_started project.
 """
 
 from os import path
+import os
+mysqlconnstr = os.environ['MYSQLCONNSTR_localdb']
+mysqlconnlst = mysqlconnstr.split(';')
+mysqlconndict = dict(s.split('=',1) for s in mysqlconnlst)
+
 PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
 DEBUG = True
